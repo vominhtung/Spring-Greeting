30199af836b81b809a8b7c64e1b58cb158cafaf4
vominhtung <vmtung5000@gmail.com>
2023-02-27 09:53:08 +0700

    TH-greeting-spring

---
+++ src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+    @GetMapping("")
+    public String greeting(@RequestParam String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Greeting</title>
+</head>
+<body>
+<h1>Hello spring mvc</h1>
+<h1>Hello: ${name}</h1>
+</body>
+</html>
